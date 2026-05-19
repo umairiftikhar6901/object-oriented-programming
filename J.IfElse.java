7c05554778f3e8ef3144367246910fc7572dda05
umairiftikhar6901 <umairiftikhar6901@gmail.com>
2026-05-19 03:31:15 -0700

    Add files via upload

---
+++ J.IfElse.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String args []){
+        
+        int number=-1;
+        if(number>0){
+            System.out.println("Positive number");
+        }
+        else{
+            System.out.println("Negative number");
+        }
+    }
+}
