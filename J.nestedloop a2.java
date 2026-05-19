836e20fc84f77513f831d4c10018d0d333f61d0e
umairiftikhar6901 <umairiftikhar6901@gmail.com>
2026-05-19 03:34:22 -0700

    Add files via upload

---
+++ J.nestedloop a2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]){
+        for(int i=3;i>=1;i--){
+            for(int j=1;j<=2;j++){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
